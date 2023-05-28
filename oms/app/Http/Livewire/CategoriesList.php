51078b47f5d3ad921fb62ecc876c3682b919c211
Vitaliy Semenov <synthex@bk.ru>
2023-05-28 11:31:13 +0600

    feat: add create category modal

--- oms/app/Http/Livewire/CategoriesList.php
+++ oms/app/Http/Livewire/CategoriesList.php
@@ -11,6 +11,15 @@ class CategoriesList extends Component
 {
     use WithPagination;
 
+    public Category $category;
+    public bool $showModal = false;
+
+    public function openModal(): void
+    {
+        $this->showModal = true;
+        $this->category = new Category();
+    }
+
     public function render(): View
     {
         $categories = Category::paginate(10);
