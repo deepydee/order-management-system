00c0063a10f732a3c2e4bf819bab6d9db098ffc0
Vitaliy Semenov <synthex@bk.ru>
2023-05-28 11:57:24 +0600

    feat: implement create category functionality

--- oms/app/Http/Livewire/CategoriesList.php
+++ oms/app/Http/Livewire/CategoriesList.php
@@ -14,15 +14,35 @@ class CategoriesList extends Component
     public Category $category;
     public bool $showModal = false;
 
+    protected function rules(): array
+    {
+        return [
+            'category.name' => ['required', 'string', 'min:3'],
+        ];
+    }
+
     public function openModal(): void
     {
         $this->showModal = true;
         $this->category = new Category();
     }
 
+    public function updatedCategoryName()
+    {
+        $this->validateOnly('category.name');
+    }
+
+    public function save()
+    {
+        $this->validate();
+        $this->category->save();
+        $this->reset('showModal');
+    }
+
     public function render(): View
     {
-        $categories = Category::paginate(10);
+        $categories = Category::latest()
+            ->paginate(10);
 
         return view('livewire.categories-list', [
             'categories' => $categories,
--- oms/resources/views/livewire/categories-list.blade.php
+++ oms/resources/views/livewire/categories-list.blade.php
@@ -91,19 +91,9 @@
                         <label class="block text-sm font-medium text-gray-700" for="category.name">
                             Name
                         </label>
-                        <input wire:model="category.name" id="category.name"
+                        <input wire:model.debounce.1000ms="category.name" id="category.name"
                                class="py-2 pr-4 pl-2 mt-2 w-full text-sm rounded-lg border border-gray-400 sm:text-base focus:outline-none focus:border-blue-400" />
                         @error('category.name')
-                            <span class="text-sm text-red-500">{{ $message }}</span>
-                        @enderror
-                    </div>
-                    <div class="mb-2 w-full">
-                        <label class="block text-sm font-medium text-gray-700" for="category.slug">
-                            Slug
-                        </label>
-                        <input wire:model="category.slug" id="category.slug"
-                               class="py-2 pr-4 pl-2 mt-2 w-full text-sm rounded-lg border border-gray-400 sm:text-base focus:outline-none focus:border-blue-400" />
-                        @error('category.slug')
                             <span class="text-sm text-red-500">{{ $message }}</span>
                         @enderror
                     </div>
